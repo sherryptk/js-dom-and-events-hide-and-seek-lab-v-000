3e0539d2a02ccc731bfd526d8e4e8a78f2a0895a
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-05-11 17:31:09 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementsByClassName('target')[0]
+}
+
+function increaseRankBy(n){
+const lis = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(n + lis[i].innerHTML)
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
